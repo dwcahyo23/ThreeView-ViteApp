de42a0b81ba5dc275160900e5bafd02d10574e92
Yusuf Dwi <dwcahyo23@gmail.com>
2024-01-15 17:09:06 +0700

    #150124

--- src/renderer/src/pages/Home.jsx
+++ src/renderer/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Center, HStack, Box } from '@chakra-ui/react'
+import { Center, Heading, Button, Box } from '@chakra-ui/react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getStatus } from '../store/liveStatus/LiveStatusSlice'
 
@@ -17,16 +17,7 @@ function Home() {
 
   return (
     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <HStack>
-        <Center w="40px" h="40px" bg="tomato" color="white">
-          <PhoneIcon />
-        </Center>
-        <Center w="40px" h="40px" bg="tomato" color="white">
-          <Box as="span" fontWeight="bold" fontSize="lg">
-            1
-          </Box>
-        </Center>
-      </HStack>
+      <Heading>HOME</Heading>
     </Box>
   )
 }
